4ce6ecf08840bb360793c43f7e16a016b3734781
Curtion <curtion@126.com>
2021-12-21 22:16:19 +0800

    更改房间id获取来源，尝试修复赠送失败问题 #50

--- src/function/send.ts
+++ src/function/send.ts
@@ -9,7 +9,6 @@ interface DyAndsid {
 }
 async function init(fans: any) {
   log.info('开始赠送')
-  // log.warn('Some problem appears')
   let DySid = await getDyAndsid()
   const sid = (DySid as DyAndsid).sid
   const dy = (DySid as DyAndsid).dy
@@ -63,10 +62,11 @@ function getDidAndRid(roomid: string) {
   return new Promise((resolve, reject) => {
     log.info('获取"' + roomid + '"房间获取did值开始')
     axios.get('https://www.douyu.com/' + roomid).then(res => {
+      let rid = res.data.match(/\$ROOM\.room_id =(.*?);/)[1].trim()
       let did = res.data.match(/owner_uid =(.*?);/)[1].trim()
       if (did !== undefined) {
         log.info('获取"' + roomid + '"房间获取did值结束，成功:' + did)
-        resolve(did)
+        resolve([did, rid !== undefined ? rid : roomid])
       } else {
         log.warn('获取"' + roomid + '"房间获取did值结束，失败')
         reject('did获取错误')
@@ -76,21 +76,27 @@ function getDidAndRid(roomid: string) {
 }
 function pushGift(payload: any) {
   return new Promise<void>((resolve, reject) => {
-    getDidAndRid(payload.rid).then((did: any) => {
+    getDidAndRid(payload.rid).then((DidAndRid: any) => {
+      const [did, rid] = DidAndRid
       setTimeout(() => {
-        log.info('赠送"' + payload.rid + '"房间开始，数量：' + payload.num)
-        Message('开始请求' + payload.rid + '房间')
+        log.info('赠送"' + rid + '"房间开始，数量：' + payload.num)
+        Message('开始请求' + rid + '房间')
         let data = new FormData()
-        data.append('rid', payload.rid)
+        data.append('rid', rid)
         data.append('prop_id', payload.prop)
         data.append('num', payload.num)
         data.append('sid', payload.sid)
         data.append('did', did)
         data.append('dy', payload.dy)
-        axios.post('https://www.douyu.com/member/prop/send', data).then((res: any) => {
-          log.info('赠送"' + payload.rid + '"房间结束，赠送结果:' + JSON.stringify(res.data))
-          resolve()
-        })
+        axios
+          .post('https://www.douyu.com/member/prop/send', data)
+          .then((res: any) => {
+            log.info('赠送"' + payload.rid + '"房间结束，赠送结果:' + JSON.stringify(res.data))
+            resolve()
+          })
+          .catch(err => {
+            reject(err)
+          })
       }, 3000)
     })
   })
